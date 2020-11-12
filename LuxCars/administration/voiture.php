dce534c1a02997fe17b991c38eec9e4e997ab9a3
Botrel Jeremiah <1942870@cgmatane.qc.ca>
2020-11-11 22:41:12 -0500

    @

---
+++ LuxCars/administration/voiture.php
@@ -0,0 +1,51 @@
+<?php
+	$idVoiture = $_GET['voiture'];
+
+	$MESSAGE_SQL_VOITURE = "SELECT * FROM voiture WHERE id = " . $idVoiture;
+
+	include "basededonnees.php";
+	$requeteListeVoiture = $basededonnees->prepare($MESSAGE_SQL_VOITURE);
+	$requeteListeVoiture->execute();
+	$voiture = $requeteListeVoiture->fetch();
+
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $voiture['marque']; ?></title>
+	<link rel="stylesheet" href="style/style.css"> 	
+</head>
+
+<body>
+	
+<header>
+	<h1><a href="liste-voiture.php" class="logo">LuxCars</a></h1>
+	<nav>
+		<h2><a href="liste-voiture.php" class="panneauAdmin">Panneau d'administration</a></h2>
+	</nav>
+</header>
+
+	<div class="contenu">
+		<div class="voiture">
+			<h3 class="marque">
+				<?php echo $voiture['marque']; ?>
+			</h3>
+			<div class="illustration img"><img src="img/<?php echo$voiture['illustration']?>" alt="voiture"></div>
+			<p class="moteur">Moteur: <?php echo $voiture['moteur']; ?></p>
+			<p class="puissance">Puissance: <?php echo $voiture['puissance']; ?></p>
+			<p class="consommation">Consommation: <?php echo $voiture['consommation']; ?></p>
+			<p class="prix">Prix: <?php echo $voiture['prix']; ?></p>
+			<p class="description"><?php echo $voiture['description']; ?></p>
+		</div>
+	
+	</div>
+	
+<footer>
+	<p>&copy;Cegep de Matane</p>
+	<p>Réalisé par Botrel Jeremiah dans cadre du cours de base de donnée</p>
+</footer>
+
+</body>
